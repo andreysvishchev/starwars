07c3e3a55e1a75c1df81fb76fe93025c7f29a903
Andrey Svishev <svishev.andrey@gmail.com>
2022-10-13 19:48:08 +0300

    add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.scss';
 import App from './app/App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store/store";
 
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-   <BrowserRouter>
+   <HashRouter>
       <Provider store={store}>
          <App/>
       </Provider>
-   </BrowserRouter>
+   </HashRouter>
 );
